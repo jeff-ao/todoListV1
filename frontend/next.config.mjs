0cdbcc614018598b5454fe64d3428f4a25611090
Jeffson <jeffsonluiz11@gmail.com>
2025-08-06 17:14:12 -0300

    config de build frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Adicione esta linha para habilitar o export estático
+  output: "export",
+
+  // Outras configurações que você possa ter...
+  // reactStrictMode: true,
+};
 
 export default nextConfig;
